a77790e9df9d6c1409a53212482e2bf2e5509a4e
Tri Ho <minhtri191193@gmail.com>
2022-02-26 16:38:40 -0800

    add: Feb 26, 2022

---
+++ Others/P1025.java
@@ -0,0 +1,20 @@
+package others;
+
+/**
+ * Date: Feb 25, 2022
+ * 1025. Divisor Game
+ * https://leetcode.com/problems/divisor-game/
+ */
+
+public class P1025 {
+    // time: O(1)
+    // space: O(1)
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
+
+/**
+ * reference
+ * https://leetcode.com/problems/divisor-game/discuss/274566/just-return-N-2-0-(proof)
+ */
