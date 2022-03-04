dae6456e6ee9e4dc1d6cc8c64b807bdd553e8f76
Tri Ho <minhtri191193@gmail.com>
2022-03-03 23:42:29 -0800

    add: March 03, 2022

---
+++ LeetCoding-Challenge/March-2022/P799.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.march2022;
+
+/**
+ * Date: March 04, 2022
+ * 799. Champagne Tower
+ * https://leetcode.com/problems/champagne-tower/
+ */
+
+public class P799 {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        return 0;
+    }
+}
