43d49c8a6d65ef507ef3ffcde0c16c9644c5520d
Tri Ho <minhtri191193@gmail.com>
2022-04-28 23:50:05 -0700

    add: Leetcode Daily Challenge April 28, 2022

---
+++ LeetCoding-Challenge/April-2022/P1631.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.april2022;
+
+/**
+ * Date: April 28, 2022
+ * 1631. Path With Minimum Effort
+ * https://leetcode.com/problems/path-with-minimum-effort/
+ */
+
+public class P1631 {
+    public int minimumEffortPath(int[][] heights) {
+        return 1;
+    }
+}
