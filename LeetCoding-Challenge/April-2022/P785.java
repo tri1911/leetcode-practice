44a5e7741eaca5d43dee7555c9e617290b4e0765
Tri Ho <minhtri191193@gmail.com>
2022-04-29 21:48:10 -0700

    add: Leetcode Daily Challenge April 29, 2022

---
+++ LeetCoding-Challenge/April-2022/P785.java
@@ -0,0 +1,15 @@
+package leetcodingchallenge.april2022;
+
+/**
+ * Date: April 29, 2022
+ * 785. Is Graph Bipartite?
+ * https://leetcode.com/problems/is-graph-bipartite/
+ */
+
+public class P785 {
+    // time:
+    // space:
+    public boolean isBipartite(int[][] graph) {
+        return true;
+    }
+}
