9e353777b649e20dc12489f3419661a6a9fc0202
Tri Ho <minhtri191193@gmail.com>
2022-04-30 22:23:39 -0700

    add: Leetcode Daily Challenge April 30, 2022

---
+++ LeetCoding-Challenge/April-2022/P399.java
@@ -0,0 +1,17 @@
+package leetcodingchallenge.april2022;
+
+import java.util.List;
+
+/**
+ * Date: April 30, 2022
+ * 399. Evaluate Division
+ * https://leetcode.com/problems/evaluate-division/
+ */
+
+public class P399 {
+    // time:
+    // space:
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        return null;
+    }
+}
