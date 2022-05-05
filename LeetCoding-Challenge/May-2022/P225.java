069428fa2ddd95e9bb9a702812019ac0889ac750
Tri Ho <minhtri191193@gmail.com>
2022-05-05 08:07:04 -0700

    add: Leetcode Daily Challenge May 05, 2022

---
+++ LeetCoding-Challenge/May-2022/P225.java
@@ -0,0 +1,43 @@
+package leetcodingchallenge.may2022;
+
+/**
+ * Date: May 05, 2022
+ * 225. Implement Stack using Queues
+ * https://leetcode.com/problems/implement-stack-using-queues/
+ */
+
+public class P225 {
+    private Deque<Integer> queue;
+
+    public P225() {
+        queue = new ArrayDeque<>();
+    }
+
+    // time: O(n)
+    // space: O(1)
+    public void push(int x) {
+        queue.add(x);
+        int n = queue.size();
+        for (int i = 0; i < n - 1; i++) queue.add(queue.remove());
+    }
+
+    // time: O(1)
+    public int pop() {
+        return queue.remove();
+    }
+
+    // time: O(1)
+    public int top() {
+        return queue.peek();
+    }
+
+    // time: O(1)
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * references
+ * https://leetcode.com/problems/implement-stack-using-queues/solution/
+ */
