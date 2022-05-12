de572bf7c19e679fed1444b5c8ee575835f0d518
Tri Ho <minhtri191193@gmail.com>
2022-05-12 15:02:19 -0700

    add: Leetcode Daily Challenge May 12, 2022

--- Algorithm-II/5-Recursion-Backtracking/P47.java
+++ Algorithm-II/5-Recursion-Backtracking/P47.java
@@ -1,19 +1,53 @@
 package algorithm2.backtracking;
 
-// Date: Dec 20, 2021
-// 47. Permutations II
-// https://leetcode.com/problems/permutations-ii/
+/**
+ * Date: Dec 20, 2021 - Redo: May 12, 2022
+ * 47. Permutations II
+ * https://leetcode.com/problems/permutations-ii/
+ */
 
 import java.util.*;
 
 // TODO: analyze the time and space complexity
-// TODO: correct the solution using sort approach
 
 public class P47 {
-    /*
-     solution with sort
-     time complexity: O(n!)
-     space complexity: O(n)
+    // redo on May 12, 2022
+
+    private List<List<Integer>> ans;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        ans = new ArrayList<>();
+        backtracking(nums, 0);
+        return ans;
+    }
+
+    private void backtracking(int[] nums, int current) {
+        int n = nums.length;
+        if (current == n - 1) {
+            List<Integer> arr = new ArrayList<>();
+            for (int num : nums) arr.add(num);
+            ans.add(arr);
+        } else {
+            boolean[] seen = new boolean[21];
+            for (int i = current; i < n; i++) {
+                if (i != current && seen[nums[i] + 10]) continue;
+                seen[nums[i] + 10] = true;
+                swap(nums, current, i);
+                backtracking(nums, current + 1);
+                swap(nums, current, i);
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = tmp;
+    }
+
+    /*  solution with sort
+        // time: O(n!)
+        // space: O(n)
         public List<List<Integer>> permuteUnique(int[] nums) {
             List<Integer> perm = new ArrayList<>();
             for (int num : nums) perm.add(num);
@@ -38,36 +72,37 @@ public class P47 {
         }
     */
 
-    // solution with Hash Map (use as a counter)
-    // time complexity: O(n!)
-    // space complexity: O(n)
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> curPerm = new ArrayList<>();
-        Map<Integer, Integer> counter = new HashMap<>();
-        for (int value : nums) {
-            counter.put(value, counter.getOrDefault(value, 0) + 1);
+    /*  solution with Hash Map (use as a counter) - done on Dec 20, 2021
+        // time complexity: O(n!)
+        // space complexity: O(n)
+        public List<List<Integer>> permuteUnique(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            List<Integer> curPerm = new ArrayList<>();
+            Map<Integer, Integer> counter = new HashMap<>();
+            for (int value : nums) {
+                counter.put(value, counter.getOrDefault(value, 0) + 1);
+            }
+            backtracking(0, nums.length, counter, curPerm, ans);
+            return ans;
         }
-        backtracking(0, nums.length, counter, curPerm, ans);
-        return ans;
-    }
 
-    private void backtracking(int current, int n, Map<Integer, Integer> counter, List<Integer> curPerm, List<List<Integer>> ans) {
-        if (current == n) {
-            ans.add(new ArrayList<>(curPerm));
-            return;
-        }
-        for (int key : counter.keySet()) {
-            int count = counter.get(key);
-            if (count != 0) {
-                curPerm.add(key);
-                counter.replace(key, count - 1);
-                backtracking(current + 1, n, counter, curPerm, ans);
-                curPerm.remove(curPerm.size() - 1);
-                counter.replace(key, count);
+        private void backtracking(int current, int n, Map<Integer, Integer> counter, List<Integer> curPerm, List<List<Integer>> ans) {
+            if (current == n) {
+                ans.add(new ArrayList<>(curPerm));
+                return;
+            }
+            for (int key : counter.keySet()) {
+                int count = counter.get(key);
+                if (count != 0) {
+                    curPerm.add(key);
+                    counter.replace(key, count - 1);
+                    backtracking(current + 1, n, counter, curPerm, ans);
+                    curPerm.remove(curPerm.size() - 1);
+                    counter.replace(key, count);
+                }
             }
         }
-    }
+     */
 
     public static void main(String[] args) {
         P47 obj = new P47();
@@ -78,5 +113,7 @@ public class P47 {
     }
 }
 
-// reference
-// https://leetcode.com/problems/permutations-ii/solution/
+/**
+ * references
+ * https://leetcode.com/problems/permutations-ii/solution/
+ */
