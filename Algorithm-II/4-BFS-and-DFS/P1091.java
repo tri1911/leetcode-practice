506f8e90419446dbf042b9c21cf17a07d7df8bfb
Tri Ho <minhtri191193@gmail.com>
2022-05-16 08:26:40 -0700

    add: Leetcode Daily Challenge May 16, 2022

--- Algorithm-II/4-BFS-and-DFS/P1091.java
+++ Algorithm-II/4-BFS-and-DFS/P1091.java
@@ -1,58 +1,57 @@
 package algorithm2.bdfAndDfs;
 
-// date: Dec 16, 2021
-// 1091. Shortest Path in Binary Matrix
-// https://leetcode.com/problems/shortest-path-in-binary-matrix/
-
-import javafx.util.Pair;
+/**
+ * Date: Dec 16, 2021 - Redo: May 16, 2022
+ * 1091. Shortest Path in Binary Matrix
+ * https://leetcode.com/problems/shortest-path-in-binary-matrix/
+ */
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-// TODO: double check for better, precise solution
-
-// using Breadth-first search approach
+// Breadth-First search approach
 public class P1091 {
-    // first attempt
-    // time complexity: O(n)
-    // space complexity: O(1)
-//    public int shortestPathBinaryMatrix(int[][] grid) {
-//        int n = grid.length;
-//        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return -1;
-//        int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
-//        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
-//        boolean[][] checked = new boolean[n][n];
-//        int[][] distances = new int[n][n];
-//        queue.add(new Pair<>(0, 0));
-//        checked[0][0] = true;
-//        distances[0][0] = 1;
-//        while (!queue.isEmpty()) {
-//            Pair<Integer, Integer> current = queue.remove();
-//            int currentRow = current.getKey();
-//            int currentCol = current.getValue();
-//            int distance = distances[currentRow][currentCol];
-//            if (currentRow == n - 1 && currentCol == n - 1)
-//                return distance;
-//            for (int i = 0; i < 8; i++) {
-//                int newRow = currentRow + directions[i][0];
-//                int newCol = currentCol + directions[i][1];
-//                if (isValid(newRow, newCol, checked, grid, n)) {
-//                    checked[newRow][newCol] = true;
-//                    distances[newRow][newCol] = distance + 1;
-//                    queue.add(new Pair<>(newRow, newCol));
-//                }
-//            }
-//        }
-//        return -1;
-//    }
+    /*  first attempt
+        // time complexity: O(n)
+        // space complexity: O(1)
+        public int shortestPathBinaryMatrix(int[][] grid) {
+            int n = grid.length;
+            if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return -1;
+            int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+            Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+            boolean[][] checked = new boolean[n][n];
+            int[][] distances = new int[n][n];
+            queue.add(new Pair<>(0, 0));
+            checked[0][0] = true;
+            distances[0][0] = 1;
+            while (!queue.isEmpty()) {
+                Pair<Integer, Integer> current = queue.remove();
+                int currentRow = current.getKey();
+                int currentCol = current.getValue();
+                int distance = distances[currentRow][currentCol];
+                if (currentRow == n - 1 && currentCol == n - 1)
+                    return distance;
+                for (int i = 0; i < 8; i++) {
+                    int newRow = currentRow + directions[i][0];
+                    int newCol = currentCol + directions[i][1];
+                    if (isValid(newRow, newCol, checked, grid, n)) {
+                        checked[newRow][newCol] = true;
+                        distances[newRow][newCol] = distance + 1;
+                        queue.add(new Pair<>(newRow, newCol));
+                    }
+                }
+            }
+            return -1;
+        }
 
-//    private boolean isValid(int row, int col, boolean[][] checked, int[][] grid, int n) {
-//        return row >= 0 && col >= 0 && row < n && col < n && grid[row][col] == 0 && !checked[row][col];
-//    }
+        private boolean isValid(int row, int col, boolean[][] checked, int[][] grid, int n) {
+            return row >= 0 && col >= 0 && row < n && col < n && grid[row][col] == 0 && !checked[row][col];
+        }
+     */
 
-    // second attempt (on Dec 20, 2021)
-    // time complexity: O(n*n)
-    // space complexity: O(1)
+    // second attempt (on Dec 20, 2021) - redo on May 16, 2022
+    // time: O(n*n)
+    // space: O(1)
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
         int[][] directions = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
