4037a7cf97e0a03961871d945173efb665da07c4
Tri Ho <minhtri191193@gmail.com>
2022-05-18 23:25:00 -0700

    add: Leetcode Daily Challenge May 18, 2022

---
+++ LeetCoding-Challenge/May-2022/P1192.java
@@ -0,0 +1,15 @@
+package leetcodingchallenge.may2022;
+
+import java.util.List;
+
+/**
+ * Date: May 18, 2022
+ * 1192. Critical Connections in a Network
+ * https://leetcode.com/problems/critical-connections-in-a-network/
+ */
+
+public class P1192 {
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        return null;
+    }
+}
