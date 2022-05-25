65807423f6810b2f2aa321013f97d6c109465f61
Tri Ho <minhtri191193@gmail.com>
2022-05-24 23:36:17 -0700

    add: Leetcode Daily Challenge May 24, 2022

---
+++ LeetCoding-Challenge/May-2022/P32.java
@@ -0,0 +1,20 @@
+package leetcodingchallenge.may2022;
+
+/**
+ * Date: May 24, 2022
+ * 32. Longest Valid Parentheses
+ * https://leetcode.com/problems/longest-valid-parentheses/
+ */
+
+public class P32 {
+    // time:
+    // space:
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
+}
+
+/**
+ * references
+ * https://leetcode.com/problems/longest-valid-parentheses/solution/
+ */
