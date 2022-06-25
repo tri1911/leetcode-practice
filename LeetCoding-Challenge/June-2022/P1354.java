c1aa9a35c6c2fed489cee7970aea1266d21d66b7
Tri Ho <minhtri191193@gmail.com>
2022-06-24 22:55:58 -0700

    add: Leetcode Daily Challenge June 24, 2022
    + 1354. Construct Target Array With Multiple Sums

---
+++ LeetCoding-Challenge/June-2022/P1354.java
@@ -0,0 +1,25 @@
+package leetcodingchallenge.june2022;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Date: June 24, 2022
+ * 1354. Construct Target Array With Multiple Sums
+ * https://leetcode.com/problems/construct-target-array-with-multiple-sums/
+ */
+
+public class P1354 {
+    // time:
+    // space:
+    public boolean isPossible(int[] target) {
+        return true;
+    }
+}
+
+/**
+ * references
+ * https://leetcode.com/problems/construct-target-array-with-multiple-sums/discuss/510256/JavaC%2B%2BPython-Backtrack-OJ-is-wrong
+ * https://leetcode.com/problems/construct-target-array-with-multiple-sums/discuss/573511/Explain-how-the-works
+ */
---
+++ LeetCoding-Challenge/June-2022/P630.java
@@ -0,0 +1,18 @@
+package leetcodingchallenge.june2022;
+
+/**
+ * Date: June 23, 2022
+ * 630. Course Schedule III
+ * https://leetcode.com/problems/course-schedule-iii/
+ */
+
+public class P630 {
+    public int scheduleCourse(int[][] courses) {
+        return 0;
+    }
+}
+
+/**
+ * references
+ * https://leetcode.com/problems/course-schedule-iii/solution/
+ */
