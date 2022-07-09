71cffcfb2abdd75bb15e451ae2129b503be4c909
Tri Ho <minhtri191193@gmail.com>
2022-07-08 22:32:19 -0700

    add: Leetcode Daily Challenge July 08, 2022
    + 1473. Paint House III

---
+++ LeetCoding-Challenge/July-2022/P1473.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.july2022;
+
+/**
+ * Date: July 08, 2022
+ * 1473. Paint House III
+ * https://leetcode.com/problems/paint-house-iii/
+ */
+
+public class P1473 {
+    public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
+        return 1;
+    }
+}
