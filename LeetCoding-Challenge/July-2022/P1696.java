9ea870bee27ea53ec105e82871fb53128a28417c
Tri Ho <minhtri191193@gmail.com>
2022-07-09 23:51:46 -0700

    add: Leetcode Daily Challenge July 09, 2022
    + 1696. Jump Game VI

---
+++ LeetCoding-Challenge/July-2022/P1696.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.july2022;
+
+/**
+ * Date: July 09, 2022
+ * 1696. Jump Game VI
+ * https://leetcode.com/problems/jump-game-vi/
+ */
+
+public class P1696 {
+    public int maxResult(int[] nums, int k) {
+        return 0;
+    }
+}
