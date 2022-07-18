1416e1cea56ac9a081a4f9ee736fb8d7de3eaffb
Tri Ho <minhtri191193@gmail.com>
2022-07-17 23:45:08 -0700

    add: Leetcode Daily Challenge July 17, 2022
    + 629. K Inverse Pairs Array

---
+++ LeetCoding-Challenge/July-2022/P629.java
@@ -0,0 +1,18 @@
+package leetcodingchallenge.july2022;
+
+/**
+ * Date: July 17, 2022
+ * 629. K Inverse Pairs Array
+ * https://leetcode.com/problems/k-inverse-pairs-array/
+ */
+
+public class P629 {
+    public int kInversePairs(int n, int k) {
+        return 0;
+    }
+}
+
+/**
+ * references
+ * https://leetcode.com/problems/k-inverse-pairs-array/solution/
+ */
