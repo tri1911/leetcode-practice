e68ae9d3416c04e0daf8f4897098396ec3e57e7e
Tri Ho <minhtri191193@gmail.com>
2022-07-18 23:15:50 -0700

    add: Leetcode Daily Challenge July 18, 2022
    + LeetCoding-Challenge/July-2022/P1074.java

---
+++ LeetCoding-Challenge/July-2022/P1074.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.july2022;
+
+/**
+ * Date: July 18, 2022
+ * 1074. Number of Submatrices That Sum to Target
+ * https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
+ */
+
+public class P1074 {
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        return 0;
+    }
+}
