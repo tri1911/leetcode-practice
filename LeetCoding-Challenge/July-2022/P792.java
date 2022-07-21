37f72767b2fe1e7e0d8f92dd0bd353b34a75794d
Tri Ho <minhtri191193@gmail.com>
2022-07-20 21:49:28 -0700

    add: Leetcode Daily Challenge July 20, 2022
    + 792. Number of Matching Subsequences

---
+++ LeetCoding-Challenge/July-2022/P792.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.july2022;
+
+/**
+ * Date: July 20, 2022
+ * 792. Number of Matching Subsequences
+ * https://leetcode.com/problems/number-of-matching-subsequences/
+ */
+
+public class P792 {
+    public int numMatchingSubseq(String s, String[] words) {
+        return 0;
+    }
+}
