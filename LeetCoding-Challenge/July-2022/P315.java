709807f42344929bd9ab8ae327880d01751ab773
Tri Ho <minhtri191193@gmail.com>
2022-07-23 21:51:39 -0700

    add: Leetcode Daily Challenge July 23, 2022
    + 315. Count of Smaller Numbers After Self

---
+++ LeetCoding-Challenge/July-2022/P315.java
@@ -0,0 +1,15 @@
+package leetcodingchallenge.july2022;
+
+import java.util.List;
+
+/**
+ * Date: July 23, 2022
+ * 315. Count of Smaller Numbers After Self
+ * https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+ */
+
+public class P315 {
+    public List<Integer> countSmaller(int[] nums) {
+        return null;
+    }
+}
