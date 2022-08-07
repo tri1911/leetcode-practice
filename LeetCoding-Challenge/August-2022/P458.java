55c336ab3bee01b2815fd888602ab24661fbd314
Tri Ho <minhtri191193@gmail.com>
2022-08-06 23:52:52 -0700

    add: Leetcode Daily Challenge August 06, 2022
    + 458. Poor Pigs

---
+++ LeetCoding-Challenge/August-2022/P458.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.august2022;
+
+/**
+ * Date: August 06, 2022
+ * 458. Poor Pigs
+ * https://leetcode.com/problems/poor-pigs/
+ */
+
+public class P458 {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return 0;
+    }
+}
