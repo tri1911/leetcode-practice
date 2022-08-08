829f94d7a044f3bba89a276e40eea6a164fddacd
Tri Ho <minhtri191193@gmail.com>
2022-08-08 00:01:22 -0700

    add: Leetcode Daily Challenge August 07, 2022
    + 1220. Count Vowels Permutation

---
+++ LeetCoding-Challenge/August-2022/P1220.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.august2022;
+
+/**
+ * Date: August 07, 2022
+ * 1220. Count Vowels Permutation
+ * https://leetcode.com/problems/count-vowels-permutation/
+ */
+
+public class P1220 {
+    public int countVowelPermutation(int n) {
+
+    }
+}
