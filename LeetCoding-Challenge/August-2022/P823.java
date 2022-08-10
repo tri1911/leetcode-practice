10fa746752a9786ed158e75ea6b972ced2ad0aee
Tri Ho <minhtri191193@gmail.com>
2022-08-09 22:51:11 -0700

    add: Leetcode Daily Challenge August 09, 2022
    + 823. Binary Trees With Factors

---
+++ LeetCoding-Challenge/August-2022/P823.java
@@ -0,0 +1,18 @@
+package leetcodingchallenge.august2022;
+
+/**
+ * Date: August 09, 2022
+ * 823. Binary Trees With Factors
+ * https://leetcode.com/problems/binary-trees-with-factors/
+ */
+
+public class P823 {
+    public int numFactoredBinaryTrees(int[] arr) {
+        return 0;
+    }
+}
+
+/**
+ * References
+ * https://leetcode.com/problems/binary-trees-with-factors/solution/
+ */
