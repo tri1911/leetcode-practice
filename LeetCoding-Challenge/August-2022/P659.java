b48f130826781be8879d5aab86784fdb98b36637
Tri Ho <minhtri191193@gmail.com>
2022-08-20 00:17:47 -0700

    add: Leetcode Daily Challenge August 19, 2022
    + 659. Split Array into Consecutive Subsequences

---
+++ LeetCoding-Challenge/August-2022/P659.java
@@ -0,0 +1,15 @@
+package leetcodingchallenge.august2022;
+
+/**
+ * Date: August 19, 2022
+ * 659. Split Array into Consecutive Subsequences
+ * https://leetcode.com/problems/split-array-into-consecutive-subsequences/
+ */
+
+public class P659 {
+    // time:
+    // space:
+    public boolean isPossible(int[] nums) {
+        return false;
+    }
+}
