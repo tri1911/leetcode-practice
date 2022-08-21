172cdc7554c59dbd52102f618c30b9c44db320f8
Tri Ho <minhtri191193@gmail.com>
2022-08-20 23:28:40 -0700

    add: Leetcode Daily Challenge August 20, 2022
    + 871. Minimum Number of Refueling Stops

---
+++ LeetCoding-Challenge/August-2022/P871.java
@@ -0,0 +1,15 @@
+package leetcodingchallenge.august2022;
+
+/**
+ * Date: August 20, 2022
+ * 871. Minimum Number of Refueling Stops
+ * https://leetcode.com/problems/minimum-number-of-refueling-stops/
+ */
+
+public class P871 {
+    // time:
+    // space:
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        return 0;
+    }
+}
