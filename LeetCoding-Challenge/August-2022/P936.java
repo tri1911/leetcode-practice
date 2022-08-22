078cb3f79c4df0aa98739b4e1b3347b341532140
Tri Ho <minhtri191193@gmail.com>
2022-08-21 23:03:40 -0700

    add: Leetcode Daily Challenge August 21, 2022
    + 936. Stamping The Sequence

---
+++ LeetCoding-Challenge/August-2022/P936.java
@@ -0,0 +1,15 @@
+package leetcodingchallenge.august2022;
+
+/**
+ * Date: August 21, 2022
+ * 936. Stamping The Sequence
+ * https://leetcode.com/problems/stamping-the-sequence/
+ */
+
+public class P936 {
+    // time:
+    // space:
+    public int[] movesToStamp(String stamp, String target) {
+        return null;
+    }
+}
