8c44e81d905c664bb1f63d60245d7a70a74d8cf5
Tri Ho <minhtri191193@gmail.com>
2022-08-27 23:49:32 -0700

    add: Leetcode Daily Challenge August 27, 2022
    + 363. Max Sum of Rectangle No Larger Than K

---
+++ LeetCoding-Challenge/August-2022/P363.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.august2022;
+
+/**
+ * Date: August 27, 2022
+ * 363. Max Sum of Rectangle No Larger Than K
+ * https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
+ */
+
+public class P363 {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        return 0;
+    }
+}
