b5c1a74c682cd0666afd53ebfe740b93301b3fb3
Tri Ho <minhtri191193@gmail.com>
2022-08-29 00:05:25 -0700

    redo: Leetcode Daily Challenge August 29, 2022
    - 200. Number of Islands

--- Algorithm-II/4-BFS-and-DFS/P200.java
+++ Algorithm-II/4-BFS-and-DFS/P200.java
@@ -1,8 +1,11 @@
 package algorithm2.bdfAndDfs;
 
-// Date: Dec 15, 2021
-// 200. Number of Islands
-// https://leetcode.com/problems/number-of-islands/
+
+/**
+ * Date: Dec 15, 2021 - Redo: August 29, 2022
+ * 200. Number of Islands
+ * https://leetcode.com/problems/number-of-islands/
+ */
 
 // TODO: double check for another solution (this is just draft solution)
 
@@ -24,28 +27,18 @@ public class P200 {
     }
 
     private static void dfs(char[][] grid, int row, int col) {
-        if (grid[row][col] != '1')  return;
+        if (grid[row][col] != '1') return;
         grid[row][col] = '0'; // or anything other than 1
-        if (row < grid.length - 1)  dfs(grid, row + 1, col); // up
-        if (col > 0)  dfs(grid, row, col - 1);// down
-        if (row > 0)  dfs(grid, row - 1, col); // right
-        if (col < grid[0].length - 1)  dfs(grid, row, col + 1); // left
+        if (row < grid.length - 1) dfs(grid, row + 1, col); // up
+        if (col > 0) dfs(grid, row, col - 1);// down
+        if (row > 0) dfs(grid, row - 1, col); // right
+        if (col < grid[0].length - 1) dfs(grid, row, col + 1); // left
     }
 
     public static void main(String[] args) {
-        char[][] grid = {
-                {'1', '1', '1', '1', '0'},
-                {'1', '1', '0', '1', '0'},
-                {'1', '1', '0', '0', '0'},
-                {'0', '0', '0', '0', '0'}
-        };
+        char[][] grid = {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}};
         System.out.println("Expect: 1. Received: " + numIslands(grid));
-        grid = new char[][]{
-                {'1', '1', '0', '0', '0'},
-                {'1', '1', '0', '0', '0'},
-                {'0', '0', '1', '0', '0'},
-                {'0', '0', '0', '1', '1'}
-        };
+        grid = new char[][]{{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}};
         System.out.println("Expect: 3. Received: " + numIslands(grid));
     }
 }
