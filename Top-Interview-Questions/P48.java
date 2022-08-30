dde7f5ee0af8fa1351fe713875bf9757f158e6d4
Tri Ho <minhtri191193@gmail.com>
2022-08-30 14:46:29 -0700

    redo: Leetcode Daily Challenge August 30, 2022
    - 48. Rotate Image

--- Top-Interview-Questions/P48.java
+++ Top-Interview-Questions/P48.java
@@ -1,14 +1,14 @@
 package topInterviewQs;
 
-// Date: Jan 8, 2022
-// 48. Rotate Image
-// https://leetcode.com/problems/rotate-image/
-
-// TODO: checkout for another implementation
+/**
+ * Date: Jan 08, 2022 - Redo: August 30, 2022
+ * 48. Rotate Image
+ * https://leetcode.com/problems/rotate-image/
+ */
 
 public class P48 {
-    // time complexity: O(M) where M is the number of cells
-    // space complexity: O(1)
+    // time: O(M) where M is the number of cells
+    // space: O(1)
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         // transpose (e.g. 0, 2 <-> 2,0 OR 2, 1 <-> 1, 2)
@@ -28,7 +28,6 @@ public class P48 {
     }
 
     // helper functions
-
     private void swap(int[][] matrix, int row1, int col1, int row2, int col2) {
         int tmp = matrix[row1][col1];
         matrix[row1][col1] = matrix[row2][col2];
@@ -47,8 +46,26 @@ public class P48 {
         }
         System.out.println(s);
     }
+
+    // Redo on August 30, 2022 - use the first approach in the official solution
+    // time: O(n) where n is the size of matrix
+    // space: O(1)
+    public void _rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - 1 - i; j++) {
+                int tmp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+    }
 }
 
-// reference
-// https://leetcode.com/problems/rotate-image/solution/
-// https://leetcode.com/problems/rotate-image/discuss/18872/A-common-method-to-rotate-the-image
+/**
+ * References
+ * https://leetcode.com/problems/rotate-image/solution/
+ * https://leetcode.com/problems/rotate-image/discuss/18872/A-common-method-to-rotate-the-image
+ */
