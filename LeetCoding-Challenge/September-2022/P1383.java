b957b340da35d25dcee1dda68538fdc29c2a8fb5
Tri Ho <minhtri191193@gmail.com>
2022-09-11 15:30:07 -0700

    add: Leetcode Daily Challenge September 11, 2022
    + 1383. Maximum Performance of a Team

---
+++ LeetCoding-Challenge/September-2022/P1383.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.september2022;
+
+/**
+ * Date: September 11, 2022
+ * 1383. Maximum Performance of a Team
+ * https://leetcode.com/problems/maximum-performance-of-a-team/
+ */
+
+public class P1383 {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        return 0;
+    }
+}
