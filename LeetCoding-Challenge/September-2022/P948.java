27387ce58e0241d3a80c2ddeb8f3e51ab75322cf
Tri Ho <minhtri191193@gmail.com>
2022-09-12 22:03:49 -0700

    add: Leetcode Daily Challenge September 12, 2022
    + 948. Bag of Tokens

---
+++ LeetCoding-Challenge/September-2022/P948.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.september2022;
+
+/**
+ * Date: September 12, 2022
+ * 948. Bag of Tokens
+ * https://leetcode.com/problems/bag-of-tokens/
+ */
+
+public class P948 {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        return 0;
+    }
+}
