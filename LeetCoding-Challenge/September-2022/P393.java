c01643ad0f60057472a5ef6289bec650b8e4ae09
Tri Ho <minhtri191193@gmail.com>
2022-09-13 23:11:09 -0700

    add: Leetcode Daily Challenge September 13, 2022
    + 393. UTF-8 Validation

---
+++ LeetCoding-Challenge/September-2022/P393.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.september2022;
+
+/**
+ * Date: September 13, 2022
+ * 393. UTF-8 Validation
+ * https://leetcode.com/problems/utf-8-validation/
+ */
+
+public class P393 {
+    public boolean validUtf8(int[] data) {
+        return false;
+    }
+}
