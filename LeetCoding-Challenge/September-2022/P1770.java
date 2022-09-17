58eeee803b905e372037cc879213deccf370bdbf
Tri Ho <minhtri191193@gmail.com>
2022-09-16 18:31:35 -0700

    add: Leetcode Daily Challenge September 16, 2022
    + 1770. Maximum Score from Performing Multiplication Operations

---
+++ LeetCoding-Challenge/September-2022/P1770.java
@@ -0,0 +1,18 @@
+package leetcodingchallenge.september2022;
+
+/**
+ * Date: September 16, 2022
+ * 1770. Maximum Score from Performing Multiplication Operations
+ * https://leetcode.com/problems/maximum-score-from-performing-multiplication-operations/
+ */
+
+public class P1770 {
+    public int maximumScore(int[] nums, int[] multipliers) {
+        return 0;
+    }
+}
+
+/**
+ * References
+ * https://leetcode.com/problems/maximum-score-from-performing-multiplication-operations/solution/
+ */
