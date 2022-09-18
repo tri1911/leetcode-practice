3c6e8c710ba3ea73dc2b33c4761ce287f1cdf7fe
Tri Ho <minhtri191193@gmail.com>
2022-09-18 15:31:45 -0700

    add: Leetcode Daily Challenge September 18, 2022
    + 42. Trapping Rain Water

--- LeetCoding-Challenge/September-2022/P336.java
+++ LeetCoding-Challenge/September-2022/P336.java
@@ -1,8 +1,20 @@
 package leetcodingchallenge.september2022;
 
+import java.util.List;
+
 /**
  * Date: September 17, 2022
+ * 336. Palindrome Pairs
+ * https://leetcode.com/problems/palindrome-pairs/
  */
 
 public class P336 {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        return null;
+    }
 }
+
+/**
+ * References
+ * https://leetcode.com/problems/palindrome-pairs/discuss/79210/The-Easy-to-unserstand-JAVA-Solution
+ */
---
+++ LeetCoding-Challenge/September-2022/P42.java
@@ -0,0 +1,13 @@
+package leetcodingchallenge.september2022;
+
+/**
+ * Date: September 18, 2022
+ * 42. Trapping Rain Water
+ * https://leetcode.com/problems/trapping-rain-water/
+ */
+
+public class P42 {
+    public int trap(int[] height) {
+        return 0;
+    }
+}
