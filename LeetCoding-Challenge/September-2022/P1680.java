f5678c9db211a6f246a50a679da236055a01f5b0
Tri Ho <minhtri191193@gmail.com>
2022-09-23 20:32:26 -0700

    add: Leetcode Daily Challenge September 23, 2022
    + 1680. Concatenation of Consecutive Binary Numbers

---
+++ LeetCoding-Challenge/September-2022/P1680.java
@@ -0,0 +1,18 @@
+package leetcodingchallenge.september2022;
+
+/**
+ * Date: September 23, 2022
+ * 1680. Concatenation of Consecutive Binary Numbers
+ * https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
+ */
+
+public class P1680 {
+    public int concatenatedBinary(int n) {
+        return 0;
+    }
+}
+
+/**
+ * References
+ *
+ */
